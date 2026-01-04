aa5c1c47a00838793934042b4e36726fe46f4935
CodeX-cmd <kuvizic.marko@gmail.com>
2026-01-04 15:27:36 +0100

    [Add] Initial maven project setup with common lib

---
+++ payment-system/ShopApp/src/test/java/com/ftn/SEP/ShopApp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.SEP.ShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
